185c3ca9706951ac29bdb384b7049a34612102c8
12Deafgyyujhegf <2431299137@qq.com>
2026-09-06 00:43:05 +0800

    第一个 Go 学习项目：Hello World + Add 函数与单元测试

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("你好，旅行者！")
+
+	// 在下面敲你自己的代码试试
+}
---
+++ math.go
@@ -0,0 +1,6 @@
+package main
+
+// Add 返回两个整数之和
+func Add(a, b int) int {
+	return a + b
+}
