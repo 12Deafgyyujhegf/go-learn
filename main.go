84106f0d2a29bd83ec8734ae62cb7a5216999f40
12Deafgyyujhegf <2431299137@qq.com>
2026-09-09 22:31:18 +0800

    pratice

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("我的第一次真实推送！")
+
+	fmt.Print("请输入你的名字")
+	var name string
+	fmt.Scan(&name)
+	fmt.Println(name)
+	fmt.Print("请输入你的年龄")
+	var age int
+	fmt.Scan(&age)
+	fmt.Println(age)
+
 }
--- math.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// Add 返回两个整数之和
-func Add(a, b int) int {
-	return a + b
-}
